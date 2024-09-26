c9a6f5be0ba054faa386782edf1941fd7752c2bd
Shivam Tiwari <114647321+MR-Shivam-Tiwari@users.noreply.github.com>
2024-09-26 17:58:26 +0530

    update

--- index.js
+++ index.js
@@ -28,11 +28,12 @@ app.use(cors({
 app.options('*', cors());
 
 app.use((req, res, next) => {
-  res.header("Access-Control-Allow-Origin", ""); // Replace '' with the domain if necessary
+  res.header("Access-Control-Allow-Origin", "*"); // Replace '' with the domain if necessary
   res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept, Authorization");
   res.header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS");
   next();
-});
+}); 
+
 
 app.use(express.json());
 app.use(express.urlencoded({ limit: '100mb', extended: true }));
