2bc43ae097c795b96ee105d48b4b7c226bf59c7d
Shivam Tiwari <114647321+MR-Shivam-Tiwari@users.noreply.github.com>
2024-09-26 17:49:31 +0530

    add cors config

--- index.js
+++ index.js
@@ -18,7 +18,22 @@ const Logs = require('./Routes/Logs');
 const PORT = process.env.PORT || 5000;
 
 const app = express();
-app.use(cors());
+app.use(cors({
+  origin: '*',
+  methods: 'GET,POST,PUT,DELETE,OPTIONS',
+  allowedHeaders: 'Content-Type,Authorization',
+  credentials: true
+}));
+
+app.options('*', cors());
+
+app.use((req, res, next) => {
+  res.header("Access-Control-Allow-Origin", ""); // Replace '' with the domain if necessary
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept, Authorization");
+  res.header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS");
+  next();
+});
+
 app.use(express.json());
 app.use(express.urlencoded({ limit: '100mb', extended: true }));
 app.use('/uploads', express.static(path.join(__dirname, 'uploads')));
